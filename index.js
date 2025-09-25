2e692867968f1c16befd5f91270c96351f961f8b
Jose-ant-1 <jgonsol1510@g.educaand.es>
2025-09-25 13:59:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,73 @@
 console.log('Happy developing ✨')
 
-const mostrar = document.getElementById("mostrar");
 let numero;
 
-do {
-    numero = Number(prompt("Introduzca el mes que quiere comprobar (entre 1 y 12)"));
-} while (isNaN(numero) || numero > 12 || numero < 1);
+let diasMeses = [31,28,31,30,31,30,31,31,30,31,30,31];
+let nombreMeses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 
-let meses = [31,28,31,30,31,30,31,31,30,31,30,31];
-let mesesCon30 = [];
-let mesesCon31 = [];
+const meses = [
+    {nombre: "Enero", dias: 31},
+    {nombre: "Febrero", dias: 28},
+    {nombre: "Marzo", dias: 31},
+    {nombre: "abril", dias: 30},
+    {nombre: "Mayo", dias: 31},
+    {nombre: "Junio", dias: 30},
+    {nombre: "Julio", dias: 31},
+    {nombre: "Agosto", dias: 31},
+    {nombre: "Septiembre", dias: 30},
+    {nombre: "Octubre", dias: 31},
+    {nombre: "Noviembre", dias: 30},
+    {nombre: "Diciembre", dias: 31}
+]
 
-meses.forEach((dias, i) => {
-    if (dias === 30) {
-        mesesCon30.push(i + 1);
-    }
-});
+let mesesCon30 = filtrarNombreMeses(30);
+let mesesCon31 = filtrarNombreMeses(31);
+let mesCon28 = filtrarNombreMeses(28);
+
+function filtrarNombreMeses(numeroDias) {
+    return meses.filter(i => i.dias === numeroDias).map(i => i.nombre);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("el documento ha cargado");
+    do {
+        numero = Number(prompt("Introduzca el mes que quiere comprobar (entre 1 y 12)"));
+    } while (!comprobarEntradaNumero(numero));
+    const texto = document.querySelectorAll("div > p");
+
+    texto[0].textContent = mesesCon31.join(", ");
+    texto[1].textContent = mesesCon30.join(", ");
 
 
-meses.forEach((dias, i) => {
-    if (dias === 31) {
-        mesesCon31.push(i + 1);
-    }
-})
-
-let mesCon28;
-meses.forEach((dias, i) => {
-    if (dias === 28) {
-        mesCon28 = (i + 1);
-    }
-})
-mostrar.innerHTML = `El mes ${numero} tiene ${meses[numero - 1]} días<br>
+    const mostrar = document.getElementById("mostrar");
+    mostrar.innerHTML = `El mes ${numero} es el mes ${nombreMeses[numero - 1]} y tiene ${diasMeses[numero - 1]} días<br>
                      Los meses con 30 días son: ${mesesCon30.join(", ")} <br>
                      Los meses con 31 días son: ${mesesCon31.join(", ")} <br>
                      El mes con 28 días es: ${mesCon28}`;
 
+});
+
+function comprobarEntradaNumero(numeroAComprobar) {
+    return !isNaN(numeroAComprobar) && numeroAComprobar >= 1 && numeroAComprobar <= 12;
+}
+
+/*
+let mesesCon30 = diasMeses
+    .map((dias, i) => dias === 30 ? i + 1 : null)
+    .filter(i => i !== null);
+let nombreMesesCon30 = mesesCon30
+    .map(i => nombreMeses[i - 1]);
+
+
+let mesesCon31 = diasMeses
+    .map((dias, i) => dias === 31 ? i + 1 : null)
+    .filter(i => i !== null);
+let nombreMesesCon31 = mesesCon31
+    .map(i => nombreMeses[i - 1]);
+
+let mesCon28 = diasMeses.findIndex(dias => dias === 28) + 1;
+let nombreMesCon28 = nombreMeses[mesCon28 - 1];
+*/
 
 /*
 if (numero === 4 || numero === 6 || numero === 9 || numero === 11) {
@@ -47,4 +80,3 @@ if (numero === 4 || numero === 6 || numero === 9 || numero === 11) {
     mostrar.innerHTML = "no se como has llegado aquí";
 }
 */
-
